96c2c6d17ea3f959200d9f6e65f0f7f56f5632bd
Rubiromi <hiromi.deyoung@gmail.com>
2015-03-30 14:37:43 -0600

    added basic scripts connection on application.slim, index page, _footer, _logo, _nav partial file.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+  def index
+    @title = 'Home'
+  #   @articles = Article.all
+  end
+  #
+  # def author
+  #   @title = "Author"
+  # end
+  #
+  # def contact
+  #   @title = "Contact me"
+  # end
+  #
+  # def software_testing_and_internationalization
+  #   @title = "Software Testing and Internationalization"
+  # end
+end
